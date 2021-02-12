391d94276e22afab6e15237e296555ba567fd819
valls <olavalls@gmail.com>
2021-02-12 13:44:15 -0300

    Criando filtro por url e cadastrando método no repository

--- src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
@@ -2,9 +2,10 @@ package br.com.alura.forum.repositories;
 
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+    List<Topico> findByCursoNome(String filterUrl);
 }
--- src/main/java/br/com/alura/forum/resources/TopicResource.java
+++ src/main/java/br/com/alura/forum/resources/TopicResource.java
@@ -4,7 +4,9 @@ import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +18,13 @@ public class TopicResource {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDTO> list() {
-        List<Topico> topicos = topicoRepository.findAll();
-        return TopicoDTO.converter(topicos);
+    public List<TopicoDTO> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDTO.converter(topicos);
+        }else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDTO.converter(topicos);
+        }
     }
 }
