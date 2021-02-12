b3298b55e9c067a49a774bed0c78cf7f3244d4b4
valls <olavalls@gmail.com>
2021-02-11 22:52:02 -0300

    Listando todos os topicos diretamente do banco com jpa repository

---
+++ src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.forum.repositories;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
--- src/main/java/br/com/alura/forum/resources/TopicResource.java
+++ src/main/java/br/com/alura/forum/resources/TopicResource.java
@@ -1,20 +1,23 @@
 package br.com.alura.forum.resources;
 
 import br.com.alura.forum.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repositories.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicResource {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDTO> list() {
-        Topico topico = new Topico("Dúvida Spring", "Erro DevTool Instalation", new Curso("Spring 1", "Programação"));
-        return TopicoDTO.converter(Arrays.asList(topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDTO.converter(topicos);
     }
 }
