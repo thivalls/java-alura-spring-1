737d991b1fb5c15a7ada89affa715c1f1ff6c81b
valls <olavalls@gmail.com>
2021-02-13 01:10:03 -0300

    Deletando

--- src/main/java/br/com/alura/forum/resources/TopicResource.java
+++ src/main/java/br/com/alura/forum/resources/TopicResource.java
@@ -8,13 +8,16 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repositories.CursoRepository;
 import br.com.alura.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -69,5 +72,11 @@ public class TopicResource {
         topicoCarregado.setTitulo(topico.getTitulo());
         topicoCarregado.setMensagem(topico.getMensagem());
         return ResponseEntity.ok().body(new TopicoFullDTO(topicoCarregado));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> destroy(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
