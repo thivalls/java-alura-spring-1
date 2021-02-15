f8af7ced29e1d32a40faf63328cdce15e425ae97
valls <olavalls@gmail.com>
2021-02-14 21:56:22 -0300

    Adicionando paginacao

--- src/main/java/br/com/alura/forum/resources/TopicResource.java
+++ src/main/java/br/com/alura/forum/resources/TopicResource.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,8 +43,13 @@ public class TopicResource {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDTO> all(@RequestParam(required = false) String nomeCurso, @RequestParam int page, @RequestParam int size) {
-        Pageable pagination = PageRequest.of(page,size);
+    public Page<TopicoDTO> all(
+            @RequestParam(required = false) String nomeCurso,
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam String order
+    ) {
+        Pageable pagination = PageRequest.of(page, size, Sort.Direction.ASC, order);
 
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(pagination);
