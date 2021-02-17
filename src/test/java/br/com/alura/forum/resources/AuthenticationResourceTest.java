9feb2d06707f616d90fd673f2d076c8b851d7bf3
valls <olavalls@gmail.com>
2021-02-17 18:48:02 -0300

    Criando arquivo application-prod.properties para build de producao

--- src/test/java/br/com/alura/forum/resources/AuthenticationResourceTest.java
+++ src/test/java/br/com/alura/forum/resources/AuthenticationResourceTest.java
@@ -1,7 +1,10 @@
 package br.com.alura.forum.resources;
 
+import br.com.alura.forum.modelo.Usuario;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -10,14 +13,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.transaction.Transactional;
 import java.net.URI;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestEntityManager
+@Transactional
 @ActiveProfiles("test")
 class AuthenticationResourceTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    public TestEntityManager em;
 
     @Test
     void deveriaRetornar400ComCredenciaisIncorretas() throws Exception {
@@ -36,6 +45,12 @@ class AuthenticationResourceTest {
     void deveriaRetornar200ComCredenciaisCorretas() throws Exception {
         URI uri = new URI("/auth");
         String json = "{ \"email\" : \"aluno@email.com\", \"password\" : \"123456\" }";
+
+        Usuario aluno = new Usuario();
+        aluno.setNome("Aluno");
+        aluno.setEmail("aluno@email.com");
+        aluno.setSenha("$2a$10$zsBm.zEwf2TZ82YLQrlCV.QGApNWnqXk8rggp2w13D3JJCwtRGlD6");
+        em.persist(aluno);
 
         mockMvc.perform(
                 MockMvcRequestBuilders
